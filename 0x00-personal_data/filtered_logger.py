170db4204c89201723ddc6a87aed0b3d5cd10360
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-03 18:45:13 +0000

    Issues Fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-''' Logger '''
+"""regex to replace a specific word in a string"""
 import re
+import os
 import logging
 from typing import List
-from os import environ
-from mysql.connector import connection
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
+    return re.sub(r'{}(?={})'.format('|'.join(fields), re.escape(separator)), redaction, message)
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 class RedactingFormatter(logging.Formatter):
@@ -17,70 +22,51 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+    def __init__(self, fields: tuple):
+        """init method"""
         self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
-        '''filters values from the log record'''
-        return filter_datum(self.fields, self.REDACTION,
-                            super().format(record), self.SEPARATOR)
-
-
-def get_db() -> connection.MySQLConnection:
-    '''returns a MySQL connector'''
-    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
-    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    db_host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = environ.get("PERSONAL_DATA_DB_NAME")
-    connector = connection.MySQLConnection(
-        user=username,
-        password=password,
-        host=db_host,
-        database=db_name)
-    return connector
+        """format method"""
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
 
 
 def get_logger() -> logging.Logger:
-    '''self descriptive'''
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
 
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
     logger.addHandler(stream_handler)
 
     return logger
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    '''returns the log message obfuscated'''
-    for field in fields:
-        message = re.sub(f'{field}=(.*?){separator}',
-                         f'{field}={redaction}{separator}', message)
-    return message
+# Connect to a secure database
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """returns a connector to the database"""
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+    return mysql.connector.connect(user=username, password=password,
+                                   host=host, database=db_name)
 
 
-def main() -> None:
-    '''driver function'''
+def main():
+    """main function"""
     db = get_db()
     cursor = db.cursor()
     cursor.execute("SELECT * FROM users;")
-
-    headers = [field[0] for field in cursor.description]
     logger = get_logger()
-
     for row in cursor:
-        info_answer = ''
-        for f, p in zip(row, headers):
-            info_answer += f'{p}={(f)}; '
-        logger.info(info_answer)
-
+        msg = "name={}; email={}; phone={}; ssn={}; password={}; ip={}; last_login={}; user_agent={}; ".format(
+            row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
+        logger.info(msg)
     cursor.close()
     db.close()
-
-
-if __name__ == '__main__':
-    main()
