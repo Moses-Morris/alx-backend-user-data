fe9ece2ca9e310216ac7576588adbd0f7120896c
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-08 10:23:15 +0000

    Life is not Basic.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,19 @@
 """ Basic Auth Class
 """
 from api.v1.auth.auth import Auth
+import typing
+import base64
 
 
 class BasicAuth(Auth):
     """ Inherits from Auth Class
     """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ Return a base 64 encoding
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if authorization_header.startswith("Basic "):
+            return "".join(authorization_header.split(" ")[1:])
