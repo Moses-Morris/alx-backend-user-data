abbdb84050cb40c65855c09c81922122c450b8ab
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-08 09:01:55 +0000

    Unauthorized response/.

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
+def unauthorized():
     """ GET /api/v1/unauthorized
     """
     return abort(401)
