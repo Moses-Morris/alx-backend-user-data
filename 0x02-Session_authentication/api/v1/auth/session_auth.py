9f0f4a5a0ae95be30da99271b7a618e2233f7dfa
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-10 20:28:44 +0000

    Suspicious.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -34,17 +34,18 @@ class SessionAuth(Auth):
         return User.get(session_id)
 
     def destroy_session(self, request=None):
-        '''self descriptive'''
-        if not request:
-            return False
-        session_cookie = self.session_cookie(request)
-
-        if not session_cookie:
+        """kill a session
+        """
+        if request is None:
             return False
 
-        user_id = self.user_id_for_session_id(session_cookie)
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
 
+        user_id = self.user_id_for_session_id(session_id)
         if not user_id:
             return False
-        self.user_id_by_session_id.pop(session_cookie)
+
+        self.user_id_by_session_id.pop(session_id, None)
         return True
