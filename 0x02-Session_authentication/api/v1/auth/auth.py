543ee121e8b906e26ebd428641827b701649883e
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-10 08:24:41 +0000

    Framed and Copied.

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" API Authentication
+"""
+from flask import request
+from typing import Tuple, List, TypeVar
+
+
+class Auth:
+    """ Class with Authentication proceedures
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Authorizes paths
+        """
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        if len(excluded_paths) == 0:
+            return True
+        if path is None or excluded_paths is None:
+            return True
+        path = path + '/' if path[-1] != '/' else path
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Authorization HTTP header
+        """
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        else:
+            return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Check and authorize user
+        """
+        return None
