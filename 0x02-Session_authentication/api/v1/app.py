10bb2a9e17687f40fc6d866329b80bd88caf0ea3
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-10 19:33:20 +0000

    Not found but not -blocked- by an authentication system.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -29,7 +29,7 @@ def before_request():
     """ Filtering of each request
     """
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth is None:
         return
     if not auth.require_auth(request.path, excluded_paths):
