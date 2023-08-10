ee89c05570d88b8202207f909c3b13ec4f7dd9a3
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-10 21:29:23 +0000

    Check Pages.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,50 +1,74 @@
 #!/usr/bin/env python3
-'''Session Auth class'''
-
-from flask import request
-from typing import List, TypeVar
-from api.v1.auth.auth import Auth
-import base64
+"""
+creating a new authentication mechanism
+"""
 import uuid
+import os
+
+
+from api.v1.auth.auth import Auth
 from models.user import User
 
 
 class SessionAuth(Auth):
-    '''self descriptive'''
+    """creating a new auth mechanism
+    """
     user_id_by_session_id = {}
+    session_user = user_id_by_session_id
 
     def create_session(self, user_id: str = None) -> str:
-        '''self descriptive'''
-        if not user_id or not isinstance(user_id, str):
+        """creates a session id for user_id
+        """
+        if user_id is None:
             return None
+        if not isinstance(user_id, str):
+            return None
+
         session_id = str(uuid.uuid4())
-        self.user_id_by_session_id[session_id] = user_id
+        self.session_user[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        '''self descriptive'''
-        if session_id and isinstance(session_id, str):
-            return self.user_id_by_session_id.get(session_id)
-        return None
+        """returns a user id based on session id
+        """
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+
+        session_value = self.session_user.get(session_id)
+        return session_value
 
     def current_user(self, request=None):
-        '''self descriptive'''
-        session_cookie = self.session_cookie(request)
-        session_id = self.user_id_for_session_id(session_cookie)
-        return User.get(session_id)
+        """an overload that returns a user instance
+        based on a cookie value"""
+        if request is None:
+            return None
+
+        cookie_value = self.session_cookie(request)
+        if not cookie_value:
+            return None
+
+        user_id = self.user_id_for_session_id(cookie_value)
+        if not user_id:
+            return None
+
+        user = User.get(user_id)
+        return user
 
     def destroy_session(self, request=None):
-        '''self descriptive'''
-        if not request:
-            return False
-        session_cookie = self.session_cookie(request)
-
-        if not session_cookie:
+        """kill a session
+        """
+        if request is None:
             return False
 
-        user_id = self.user_id_for_session_id(session_cookie)
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
 
+        user_id = self.user_id_for_session_id(session_id)
         if not user_id:
             return False
-        self.user_id_by_session_id.pop(session_cookie)
+
+        self.user_id_by_session_id.pop(session_id, None)
         return True
