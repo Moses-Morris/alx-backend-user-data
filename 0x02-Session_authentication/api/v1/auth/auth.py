589ca53d24663adc6f10c2b267b44da0e406da80
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-10 08:34:11 +0000

    Copy all files to new folder.

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -9,19 +9,30 @@ class Auth:
     """ Class with Authentication proceedures
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Authorizes paths
+        """
+        Determines whether a given path requires authentication or not
+        Args:
+            - path(str): Url path to be checked
+            - excluded_paths(List of str): List of paths that do not require
+              authentication
+        Return:
+            - True if path is not in excluded_paths, else False
         """
         if path is None:
             return True
-        if excluded_paths is None:
+        elif excluded_paths is None or excluded_paths == []:
             return True
-        if len(excluded_paths) == 0:
-            return True
-        if path is None or excluded_paths is None:
-            return True
-        path = path + '/' if path[-1] != '/' else path
-        if path in excluded_paths:
+        elif path in excluded_paths:
             return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
         return True
 
     def authorization_header(self, request=None) -> str:
