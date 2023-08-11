563c48c6992bcb43c4bf9fc8611e3784b6738775
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-11 08:20:48 +0000

    Copy work and modify.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -25,16 +25,18 @@ if os.getenv("AUTH_TYPE") == "basic_auth":
 def before_request():
     """ Filtering of each request
     """
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-    if auth is None:
-        return
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+    if auth is not None:
+        excluded = ['/api/v1/status/',
+                    '/api/v1/unauthorized/',
+                    '/api/v1/forbidden/',
+                    '/api/v1/auth_session/login/']
+        if auth.require_auth(request.path, excluded):
+            if (auth.authorization_header(request) is None and
+                    auth.session_cookie(request) is None):
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
+            request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
