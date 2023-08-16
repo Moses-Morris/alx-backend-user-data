73949322746288115e83562316eb0f894e1ebd73
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-16 09:39:44 +0000

    Create  a hashed password using bcrypt.

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" The Auth and password protection """
+
+import bcrypt
+
+
+def _hash_password(password: str):
+    """ Salted hash """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
