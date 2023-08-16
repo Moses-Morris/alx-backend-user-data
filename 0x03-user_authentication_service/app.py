9c6880996ef417a425eebe5343bcb49b91b77c73
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-16 10:47:23 +0000

    Great requests from forms and user information.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """ Good pins for a Flask App """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -10,6 +14,22 @@ app = Flask(__name__)
 def payload() -> str:
     """returning a jsonified string"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'])
+def users():
+    """ Get user data from a form """
+    try:
+        data = request.form
+        email = data.get('email')
+        password = data.get('password')
+
+        user = AUTH.register_user(email, password)
+        response = {"email": user.email, "message": "user created"}
+        return jsonify(response), 200
+    except ValueError as e:
+        response = {"message": "email already registered"}
+        return jsonify(response), 400
 
 
 if __name__ == "__main__":
