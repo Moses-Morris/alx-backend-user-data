f9370f487e6ee74ea6334c824a4fddfb569a8920
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-16 10:04:43 +0000

    register user.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ The Auth and password protection """
 import bcrypt
+from user import User
+from db import DB
 
 
 def _hash_password(password: str) -> str:
@@ -8,3 +10,23 @@ def _hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> bool:
+        if email and password:
+            user_exist = self._db._session.query(User).filter_by(email=email).first()
+            if user_exist:
+                hashed = user_exist.hashed_password
+                if bcrypt.checkpw(password.encode('utf-8'), hashed):
+                    return True
+                else:
+                    return False
+            else:
+                return False
