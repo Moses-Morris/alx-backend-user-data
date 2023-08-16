cf9a78745a018e71994e7509cb916873e90f5478
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-16 09:42:29 +0000

    Fix : Hash a password using bycrypt and gensalt()

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """ The Auth and password protection """
-
 import bcrypt
 
 
-def _hash_password(password: str):
+def _hash_password(password: str) -> str:
     """ Salted hash """
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
