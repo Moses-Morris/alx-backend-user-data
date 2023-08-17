2a0fcffbcad9193fbd257b419f1523d6d68c9a13
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-17 09:25:04 +0000

    Responding to session created.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -37,14 +37,19 @@ def login():
     """ Login sessions Response """
     data = request.form
     email = data.get('email')
-    password = data.get('password')
-    if not AUTH.valid_login(email, password):
+    passwd = data.get('password')
+
+    if not AUTH.valid_login(email, passwd):
         abort(401)
-    else:
-        session_id = AUTH.create_session(email)
-        response = jsonify({"email": email, "message": "logged in"})
-        response.set_cookie('session_id', session_id)
-        return response
+
+    session_id = AUTH.create_session(email)
+
+    response = make_response(jsonify({
+        "email": email,
+        "message": "logged in"}))
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
