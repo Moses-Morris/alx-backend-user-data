c09170a19c05c5d306a8105c3a10a361413e6c28
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-17 09:47:25 +0000

    Find user by session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,16 +56,12 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
-    def get_user_from_session_id(session_id: str) -> User:
+    def get_user_from_session_id(self, session_id: str) -> User:
         """ Get user from session ID """
-        if session_id is None:
+        if not session_id:
             return None
-
-        try:
-            user = self._db.find_user_by(session_id=session_id)
-            if not user or user is None:
-                return None
-
+        user = self._db.find_user_by(session_id=session_id)
+        if user:
             return user
-        except NoResultFound:
+        else:
             return None
