e3bdd3fc0b339adeb030b5fbdf8128737e9d09e4
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-17 15:20:03 +0000

    Generate reset password token.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -74,3 +74,17 @@ class Auth:
         """ Destroy User Session """
         if user_id:
             self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Generate reset password token """
+        try:
+            user = self._db.find_user_by(email=email)
+            if not user or user is None:
+                raise ValueError("No user Found")
+
+            reset_token = _generate_uuid()
+            user.reset_token = reset_token
+        except NoResultFound:
+            raise ValueError("No user Found")
+        else:
+            return reset_token
