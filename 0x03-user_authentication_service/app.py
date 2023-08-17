39e5400302b5aba694323ed1e92b7a2e77731f6b
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-17 09:22:55 +0000

    Create a login session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -32,5 +32,20 @@ def users():
         return jsonify(response), 400
 
 
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
+def login():
+    """ Login sessions Response """
+    data = request.form
+    email = data.get('email')
+    password = data.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    else:
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
